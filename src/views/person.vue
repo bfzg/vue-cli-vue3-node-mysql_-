44ae6ab4e7f35a20d17e195de1652b5f93b1ce1c
bfzg <13075035453@163.com>
2022-11-24 21:55:24 +0800

    修改了个人活动页面

--- src/views/person.vue
+++ src/views/person.vue
@@ -10,28 +10,24 @@
            :modules="modules"
            class="mySwiper"
        >
-         <swiper-slide>
+         <swiper-slide v-for="(item,index) in list" :key="index">
            <div class="tbox">
-             <h5>标题</h5>
+             <h5>{{ item.title }}</h5>
            </div>
            <br>
              <div class="ibox">
-
                <img src="./1.jpg" alt="">
-
                <div class="itext">
-               <h5>yuan</h5>
-               <span>邮箱:1445237848</span>
+               <h5>发布人:{{item.sponsor}}</h5>
+               <span>邮箱:{{item.emali}}</span>
                </div>
              </div>
            <div class="content">
              <div>
-               <span>&nbsp;&nbsp; fdsnifdhsaioufhdshfdoishfodshfiodshaiofhdoshfodshaoifhdosiahfodshfoidshafiodhsoiafhdsoiafhwoihfoiewhfiowehfoiehfoiweh</span>
+               <span>&nbsp;&nbsp;{{item.content}}</span>
              </div>
            </div>
          </swiper-slide>
-         <swiper-slide>Slide 2</swiper-slide>
-         <swiper-slide>Slide 3</swiper-slide>
        </swiper>
      </div>
    </section>
@@ -55,8 +51,16 @@ export default {
     SwiperSlide,
   },
   setup() {
+    let list = [
+      {title:'校园摄影大赛',content:'本次比赛所有在校生均可参赛sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss，比赛地点在校内，作品提交为线上提交，评审团：张三，李四，王二，',department:'机电信息系',sponsor:'元',emali:'1445237848@163.com'},
+      {title:'校园摄影大赛',content:'本次比赛所有在校生均可参赛sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss，比赛地点在校内，作品提交为线上提交，评审团：张三，李四，王二，',department:'机电信息系',sponsor:'元',emali:'1445237848@163.com'},
+      {title:'校园摄影大赛',content:'本次比赛所有在校生均可参赛sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss，比赛地点在校内，作品提交为线上提交，评审团：张三，李四，王二，',department:'机电信息系',sponsor:'元',emali:'1445237848@163.com'},
+      {title:'校园摄影大赛',content:'本次比赛所有在校生均可参赛sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss，比赛地点在校内，作品提交为线上提交，评审团：张三，李四，王二，',department:'机电信息系',sponsor:'元',emali:'1445237848@163.com'},
+      {title:'校园摄影大赛',content:'本次比赛所有在校生均可参赛sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss，比赛地点在校内，作品提交为线上提交，评审团：张三，李四，王二，',department:'机电信息系',sponsor:'元',emali:'1445237848@163.com'},
+    ]
     return {
       modules: [EffectCards],
+      list
     };
   },
 }
