4c5bc4446e3f2a44f1011712b54672f94ce5b701
bfzg <13075035453@163.com>
2023-03-04 16:31:36 +0800

    完成了子级评论渲染 发布 查看图片 隐藏导航栏 scroll有点问题

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,17 @@ import user from './modules/user'
 import dialogBox from "@/store/modules/dialogBox";
 export default createStore({
   state: {
+    previewPictureShow:false
   },
   getters: {
   },
   mutations: {
+    //控制查看图片组件的显示与隐藏
+    changePreviewPictureShow(state,value){
+      state.previewPictureShow = value;
+      console.log( state.previewPictureShow)
+      return;
+    }
   },
   actions: {
   },
